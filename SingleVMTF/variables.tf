08a41e3b4823d7307b9e2249630a01d54ef5f653
owaispathan <45031985+owaispathan@users.noreply.github.com>
2024-07-14 12:21:25 +0300

    Add files via upload

---
+++ SingleVMTF/variables.tf
@@ -0,0 +1,30 @@
+variable "vm_name" {
+  description = "Name of the virtual machine"
+  type        = string
+}
+
+variable "vm_size" {
+  description = "Size of the virtual machine"
+  type        = string
+  default     = "Standard_DS1_v2"
+}
+
+variable "location" {
+  description = "Azure region where the resources will be deployed"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group to deploy resources into"
+  type        = string
+}
+
+variable "vnet_name" {
+  description = "Name of the virtual network"
+  type        = string
+}
+
+variable "subnet_name" {
+  description = "Name of the subnet within the virtual network"
+  type        = string
+}
